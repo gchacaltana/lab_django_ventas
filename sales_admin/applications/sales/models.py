351abe83ce24871877e59fccda99f349ad9a2860
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2022-10-02 23:25:43 -0500

    update models warehouse

--- sales_admin/applications/sales/models.py
+++ sales_admin/applications/sales/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Currency(models.Model):
+    """
+    Clase Moneda.
+    Ejemplo 1: Moneda Sol
+    Código: PEN
+    Simbolo: S/.
+    Nombre: Sol Peruano
+
+    Ejemplo 2: Moneda Dólares Americanos.
+    Código: USD
+    Simbolo: $.
+    Nombre: Dólares Americanos
+    """
+
+    id = models.AutoField(primary_key=True)
+
+    code = models.CharField(max_length=3,unique=True, verbose_name="Código")
+
+    symbol = models.CharField(max_length=4, verbose_name="Simbolo")
+
+    name = models.CharField(max_length=20, verbose_name="Nombre")
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Fecha de Moficación")
+
+    def __str__(self):
+        return f"{self.symbol} {self.code}"
+
+    class Meta:
+        db_table = "currency"
+        verbose_name = "Moneda"
