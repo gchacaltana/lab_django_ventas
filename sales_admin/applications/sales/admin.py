83dfd37326728b6f655a35b0151836114cfdb87f
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2022-10-02 23:52:13 -0500

    update sales files

--- sales_admin/applications/sales/admin.py
+++ sales_admin/applications/sales/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from applications.sales.models import Currency
+
+# Agregamos al modelo Currency al django admin
+admin.site.register(Currency)
--- sales_admin/applications/sales/apps.py
+++ sales_admin/applications/sales/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SalesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'applications.sales'
+    verbose_name = "Mod. Ventas"
